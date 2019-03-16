3b1fdfe455b60bbfda33faf99e39270df37d43a1
ryubb <ryubb1108@gmail.com>
2019-03-16 18:08:56 +0900

    reduxを用いて、簡単なtodoアプリの作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { addToDo, removeToDo } from './actions';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,10 +27,42 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <ul>
+          {this.props.todos.map(todo => {
+            return (
+              <li key={todo}>
+                <span>{todo}</span>
+                <button onClick={() => this.props.onRemoveToDo(todo)}>削除</button>
+              </li>
+            )
+          })}
+        </ul>
+        <input type="text" onChange={e => this.setState({input: e.target.value})} />
+        <button onClick={() => this.props.onAddToDo(this.state.input)}>
+          追加
+        </button>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddToDo(todo) {
+      dispatch(addToDo(todo))
+    },
+    onRemoveToDo(todo) {
+      dispatch(removeToDo(todo))
+    }
+  }
+};
+
+// これを記述することで、this.props.dispatchが使える？
+const mapStateToProps = state => {
+  return {
+    todos: state.todos.list
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
